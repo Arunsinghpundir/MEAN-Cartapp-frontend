c695378fa48987efae89c2d1adf3fbb3a5c7e2bc
Vaun <varunrana814@gmail.com>
2024-03-20 20:08:39 +0530

    login added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  form: FormGroup;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: '',
+      password: '',
+    });
+  }
+
+  validateEmail(email: string): boolean {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailRegex.test(email);
+  }
+  submit(): void {
+
+    const userData = this.form.value;
+      console.log(this.validateEmail(userData.email))
+      if (userData.password === "" || userData.email === "") {
+        console.log(userData.email)
+        Swal.fire("Error", "Please enter all required fields");
+      } else if (!this.validateEmail(userData.email)) {
+        Swal.fire("Error", "Invalid email address");
+      } else {
+        this.http.post("http://localhost:5000/api/login",userData,{
+          withCredentials:true
+        })
+        .subscribe(
+          (res)=> this.router.navigate(['/']),
+          (err)=> {
+            Swal.fire("Error",err.error.message,"error")
+          }
+        )
+      }
+  }
 }
