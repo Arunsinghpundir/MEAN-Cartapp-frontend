620f64b8b86cd385f16e999b25b773f07301efb1
Arun Singh Pundir <arunsinghpundir.325@gmail.com>
2024-03-20 23:44:46 +0530

    product added

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,3 +1,27 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Component, OnInit } from '@angular/core';
+// import { Emitters } from '../../emitters/emitter';
+
+// @Component({
+//   selector: 'app-nav',
+//   templateUrl: './nav.component.html',
+//   styleUrl: './nav.component.css'
+// })
+// export class NavComponent implements OnInit {
+//   authenticated = false;
+
+//   constructor(private http:HttpClient){}
+
+//   ngOnInit():void {
+//     Emitters.authEmitter.subscribe((auth:boolean)=>{
+//       this.authenticated = auth;
+//     })
+//   }
+//   logout():void {
+//     this.http.post('http://localhost:5000/api/logout',{},{withCredentials:true})
+//     .subscribe(()=> this.authenticated = false)
+//   }
+// }
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Emitters } from '../../emitters/emitter';
@@ -5,20 +29,21 @@ import { Emitters } from '../../emitters/emitter';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrls: ['./nav.component.css'] // Fix typo in styleUrls
 })
 export class NavComponent implements OnInit {
   authenticated = false;
 
-  constructor(private http:HttpClient){}
+  constructor(private http: HttpClient) { }
 
-  ngOnInit():void {
-    Emitters.authEmitter.subscribe((auth:boolean)=>{
+  ngOnInit(): void {
+    Emitters.authEmitter.subscribe((auth: boolean) => {
       this.authenticated = auth;
-    })
+    });
   }
-  logout():void {
-    this.http.post('http://localhost:5000/api/logout',{},{withCredentials:true})
-    .subscribe(()=> this.authenticated = false)
+
+  logout(): void {
+    this.http.post('http://localhost:5000/api/logout', {}, { withCredentials: true })
+      .subscribe(() => this.authenticated = false);
   }
 }
--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
 
+
+// 
+
+// }
+
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Emitters } from '../../emitters/emitter';
+
+import Swal from 'sweetalert2';
 @Component({
-  selector: 'app-products',
-  templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
-})
-export class ProductsComponent {
+    selector: 'app-products',
+    templateUrl: './products.component.html',
+    styleUrl: './products.component.css'
+  })
+  export class ProductsComponent implements OnInit {
+  products: any[];
 
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+  url:string = 'https://mean-cartapp-backend.onrender.com/api/user';
+  localurl = 'http://localhost:5000/api/products';
+  authenticated = false;
+  ngOnInit(): void {
+    Emitters.authEmitter.subscribe((auth: boolean) => {
+      this.authenticated = auth;
+    });
+
+    this.http
+    .get(this.localurl, {
+      withCredentials: true
+    })
+    .subscribe(
+      async (response: any) => {
+       
+        this.products = response;
+        console.log(response);
+        Emitters.authEmitter.emit(true);
+      },
+      (err) => {
+     
+        Emitters.authEmitter.emit(false);
+      }
+    );
+  }
 }
